c31ff46e2baf2ab4201d60e7ca65bdb3a8657877
profbrennan <68320874+profbrennan@users.noreply.github.com>
2026-04-27 14:50:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 
 const SUPABASE_URL  = 'https://dxbyocmnvtrnsgtfdjhq.supabase.co';
 // const SUPABASE_ANON = 'sb_publishable_VQpPSdbokPzddFcss1hcYg_aVDVEQNc';
-const SUPABASE_ANON = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4YnlvY21udnRybnNndGZkamhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyODQzOTUsImV4cCI6MjA5Mjg2MDM5NX0.37WfslVF-083s8uZaqioa2Pk_tzh4ky9CIGzmXV5aOU
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4YnlvY21udnRybnNndGZkamhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyODQzOTUsImV4cCI6MjA5Mjg2MDM5NX0.37WfslVF-083s8uZaqioa2Pk_tzh4ky9CIGzmXV5aOU';
 
 // Prices in GBP
 const PRICE_SINGLE = 0.50;
